ed4b43b878a71fd84f73f9ecad236675b3677273
Josh Singer <josh.j.singer@gmail.com>
2017-08-04 00:22:37 -0500

    init again

---
+++ SpacegameReloaded/GameScene.swift
@@ -0,0 +1,68 @@
+//
+//  GameScene.swift
+//  SpacegameReloaded
+//
+//  Created by Training on 01/10/2016.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+import CoreMotion
+
+class GameScene: SKScene {
+ 
+    
+    override func didMove(to view: SKView) {
+        
+        
+        
+        
+    }
+    
+    
+    
+    func addAlien () {
+        
+    
+    }
+    
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    
+    func fireTorpedo() {
+        
+        
+        
+    }
+    
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+    }
+    
+    
+    func torpedoDidCollideWithAlien (torpedoNode:SKSpriteNode, alienNode:SKSpriteNode) {
+    
+        
+        
+    }
+    
+    override func didSimulatePhysics() {
+        
+       
+        
+    }
+    
+    
+    
+    
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
